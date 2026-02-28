f74adf7c104c9595832d8e14be65f5bcab78a56a
kevintimoteovasquez0-create <kevintimoteovasquez0@gmail.com>
2026-02-28 16:01:54 -0500

    Cambio

--- src/empleado/dto/create-empleado.dto.ts
+++ src/empleado/dto/create-empleado.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { Transform } from "class-transformer";
-import { IsDateString, IsEmail, IsEnum, IsInt, IsNotEmpty, IsOptional, IsPositive, IsString, Length, Matches, MaxLength, ValidateIf } from "class-validator";
+import { IsDate, IsDateString, IsEmail, IsEnum, IsInt, IsNotEmpty, IsOptional, IsPositive, IsString, Length, Matches, MaxLength, ValidateIf } from "class-validator";
 
 export enum DocumentoEnumDto {
   DNI = 'dni',
@@ -112,9 +112,8 @@ export class CreateEmpleadoDto {
     format: 'date',
   })
   @IsString()
-  @Matches(/^\d{4}-\d{2}-\d{2}$/, { 
-    message: "El formato de la fecha de ingreso no es válido.", 
-  })
+  @IsDateString({}, { 
+    message: "El formato de la fecha de ingreso no es válido.", })
   @IsNotEmpty({ message: "La fecha de ingreso es un campo requerido." })
   fecha_ingreso: string;
 
@@ -124,7 +123,7 @@ export class CreateEmpleadoDto {
     type: String,
     format: 'date',
   })
-  @Transform(({ value }) => value + 'T00:00:00:00Z')
+  @IsString()
   @IsDateString({}, { message: "El formato de la fecha de nacimiento no es válido." })
   @IsNotEmpty({ message: "La fecha de nacimiento es un campo requerido." })
   fecha_nacimiento: string;
